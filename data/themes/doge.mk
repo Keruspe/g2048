d2ac0539a16ff11f0e1dc84c3d4a0412f2109cb2
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2014-03-27 22:22:13 +0900

    install doge theme
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

---
+++ data/themes/doge.mk
@@ -0,0 +1,34 @@
+# This file is part of g2048.
+#
+# Copyright 2014 Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
+#
+# g2048 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# g2048 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with g2048.  If not, see <http://www.gnu.org/licenses/>.
+
+
+dogethemedir = $(themesdir)/doge
+
+dogetheme_DATA = \
+	data/themes/doge/0
+	data/themes/doge/2
+	data/themes/doge/4
+	data/themes/doge/8
+	data/themes/doge/16
+	data/themes/doge/32
+	data/themes/doge/64
+	data/themes/doge/128
+	data/themes/doge/256
+	data/themes/doge/512
+	data/themes/doge/1024
+	data/themes/doge/2048
+	$(NULL)
